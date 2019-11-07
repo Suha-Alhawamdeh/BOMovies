107b3b724546d1792b90e1e5e351562d709b10dc
Suha-Alhawamdeh <soso.slman@hotmail.com>
2019-11-07 05:25:40 +0200

    adding js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
 
 document.write('<h1>' + 'Welcome in our website' + '</h1>');
 
-
 var txt ;
 
 function confirmfunc() {
@@ -12,9 +11,24 @@ function confirmfunc() {
     document.write(txt);
   } else {
     txt = 'Unfortunatly, we looking forward to intertnment you next time';
-    document.write('<h1>' + txt + '</h1>');
   }
 }
 
 
 confirmfunc();
+
+function createGreeting(){
+  var hourNow = prompt('what time is now?');
+  var greeting;
+  if (hourNow > 18 ) {
+    greeting = 'Good evening, Class!';}
+  else if (hourNow > 12 ) {
+    greeting = 'Good afternoon, Class!';}
+  else if (hourNow >= 0 ) {
+    greeting = 'Good morning!';
+    greeting = 'Welcome!';}
+  return greeting;
+}
+
+document.write('<h1>' + greeting + '</h1>');
+document.write('<h1>' + txt + '</h1>');
